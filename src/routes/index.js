4f5b1dd0c022392ffe4bee87afcadda3840674d9
xlarp <freedom@xvh.lol>
2025-01-26 15:10:04 +0200

    Add health check endpoint and update API routes to include versioning

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,15 +8,19 @@ const {
   dateController,
 } = require("../controllers");
 const apiKeyMiddleware = require("../middlewares/auth");
+const responses = require("../responses");
 
 function setupRoutes(app) {
-  app.use(apiKeyMiddleware);
+  app.get("/v1/health", (req, res) => {
+    return res.json(responses.successResponse("API is healthy"));
+  });
 
-  app.use("/api/search", searchController);
-  app.use("/api/random", randomController);
-  app.use("/api/browse", browseController);
-  app.use("/api/author", authorController);
-  app.use("/api/date", dateController);
+  app.use(apiKeyMiddleware);
+  app.use("/v1/api/search", searchController);
+  app.use("/v1/api/random", randomController);
+  app.use("/v1/api/browse", browseController);
+  app.use("/v1/api/author", authorController);
+  app.use("/v1/api/date", dateController);
 
   app.all("*", (req, res) => {
     return res.status(400).json(badRequestResponse("Invalid endpoint"));
